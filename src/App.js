4e34c91240e24546df171df48274668d1e41eb1a
syedenayetali <enayetali2000@gmail.com>
2023-02-15 21:32:37 +0530

    First Change

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Using this react app to deploy to Netlify and making it live</p>
+        <p> To learn the process of deployment and pipelining.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
